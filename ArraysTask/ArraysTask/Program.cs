e3f7657b51fe77b40cbb9176d3647f6c8cf835b7
KarlOntol <rafi-pxp@mail.ru>
2022-08-19 21:09:53 +0300

    Update (0)

---
+++ ArraysTask/ArraysTask/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ArraysTask
+{
+    class Program
+    {
+        static void Main()
+        {
+            Apple(200, 8);
+            Numbers();
+
+        }
+
+        static void Apple(double ter, byte year)
+        {
+            for (byte i = 1; i <= year; i++)
+            {
+                ter = ter * 1.1;
+            }
+            Console.WriteLine(ter);
+        }
+
+        static void Numbers()
+        {
+            foreach (int i in Enumerable.Range(0, 100))
+            {
+                if (i % 8 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            };
+        }
+     
+    }
+}
+
+
+
